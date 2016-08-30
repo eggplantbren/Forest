4a4e7dd9b4987c4bedab07ff25dd6ab67becc632
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-08-30 14:54:32 +1200

    Posterior for N

--- code/cpp/display.py
+++ code/cpp/display.py
@@ -1,4 +1,5 @@
 import dnest4.classic as dn4
+import numpy as np
 import matplotlib.pyplot as plt
 
 data = dn4.my_loadtxt("toy_data.txt")
@@ -12,5 +13,13 @@ num = min([50, posterior_sample.shape[0]])
 for i in range(0, num):
     plt.plot(data[:,0], posterior_sample[i, 0:data.shape[0]], "g",\
                     alpha=0.1)
+plt.show()
+
+# Posterior for N
+N = posterior_sample[:,109]
+bins = np.arange(0, 101)
+width = 0.3
+plt.hist(N, bins=bins-0.5*width, width=width, color="k", alpha=0.2)
+plt.xlim([-0.5, 100.5])
 plt.show()
 
