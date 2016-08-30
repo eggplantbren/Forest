cc06c7a4ad818201874abedb963d9f1bedd51d96
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-08-30 14:07:58 +1200

    Bugfix

--- code/cpp/display.py
+++ code/cpp/display.py
@@ -7,10 +7,10 @@ posterior_sample = dn4.my_loadtxt("posterior_sample.txt")
 plt.errorbar(data[:,0], data[:,1], yerr=data[:,2], fmt="ko")
 plt.hold(True)
 
-# Overplot first 100 posterior samples
-num = min([100, posterior_sample.shape[0]])
+# Overplot first 50 posterior samples
+num = min([50, posterior_sample.shape[0]])
 for i in range(0, num):
-    plt.plot(data[:,0], posterior_sample[0, 0:data.shape[0]], "g",\
-                    alpha=0.2)
+    plt.plot(data[:,0], posterior_sample[i, 0:data.shape[0]], "g",\
+                    alpha=0.1)
 plt.show()
 
